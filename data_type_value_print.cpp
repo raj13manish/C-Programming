40171a51ecd477f09f60c0ebae46092768c3611d
raj13manish <rajm95518@gmail.com>
2023-02-01 21:02:58 +0530

    data print

---
+++ data_type_value_print.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    int a;
+    float b;
+    char c;
+cout<<"enter the value of a";
+cin>>a;
+cout<<"the valu of a is =";
+cout<<a<<endl;    
+cout<<"enter the value of b";
+cin>>b;
+cout<<"the valu of b is =";
+cout<<b<<endl; 
+cout<<"enter the value of c";
+cin>>c;
+cout<<"the valu of c is =";
+cout<<c<<endl;  
+return 0;   
+}
+  
